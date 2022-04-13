9dad74ac4ae023479eb2b4b46aae5c0ab03dfb09
alexs <alexsanderstrom@outlook.com>
2022-04-13 19:04:04 +0200

    Lade till onclick event som skickar namn till nästa intent

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent intent;
+    private EditText whatToSearchFor;
+    private String whatToSearchForString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button search = findViewById(R.id.search_button);
+        whatToSearchFor = findViewById(R.id.input_name);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProfilePage.class);
+                whatToSearchForString = whatToSearchFor.getText().toString();
+                intent.putExtra("name", whatToSearchForString);
+                startActivity(intent);
+            }
+        });
     }
+
 }
