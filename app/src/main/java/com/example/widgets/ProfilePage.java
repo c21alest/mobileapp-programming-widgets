b56ecc4e5d9634b3f24dc80cdc06b1dc3ae659bf
alexs <alexsanderstrom@outlook.com>
2022-04-13 19:08:19 +0200

    Lade till bundle som behandlar och sidan visar extras från tidigare intent i textruta

--- app/src/main/java/com/example/widgets/ProfilePage.java
+++ app/src/main/java/com/example/widgets/ProfilePage.java
@@ -3,6 +3,8 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ProfilePage extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class ProfilePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        TextView person = findViewById(R.id.person_field);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("name");
+            person.setText(name);
+        }
     }
 }
