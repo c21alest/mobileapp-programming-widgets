f1e32a1caa956d2ec4ee61b35f616a7459eeca05
alexs <alexsanderstrom@outlook.com>
2022-04-13 19:17:16 +0200

    Lade till kod som skapar exempelbild för profilbilden

--- app/src/main/java/com/example/widgets/ProfilePage.java
+++ app/src/main/java/com/example/widgets/ProfilePage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProfilePage extends AppCompatActivity {
@@ -14,11 +15,14 @@ public class ProfilePage extends AppCompatActivity {
         setContentView(R.layout.activity_profile_page);
 
         TextView person = findViewById(R.id.person_field);
+        ImageView ProfilePicture = findViewById(R.id.imageView);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String name = extras.getString("name");
             person.setText(name);
         }
+
+        ProfilePicture.setImageResource(R.drawable.ic_launcher_foreground);
     }
 }
